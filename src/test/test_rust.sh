8cc61a3293f69ba8367132fbc5299423ee07ce13
Alex Xu (Hello71) <alex_y_xu@yahoo.ca>
2018-02-28 10:28:49 -0500

    Fix Rust -> C FFI tests.
    Also add a useless protover test to test tests.

--- src/test/test_rust.sh
+++ src/test/test_rust.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-# Test all Rust crates
 
-set -e
-
-CARGO_TARGET_DIR="${abs_top_builddir:-../../..}/src/rust/target" \
-    CARGO_HOME="${abs_top_builddir:-../../..}/src/rust" \
-    find "${abs_top_srcdir:-../../..}/src/rust" \
-    -mindepth 2 -maxdepth 2 \
-    -type f -name 'Cargo.toml' \
-    -exec "${CARGO:-cargo}" test --all-features ${CARGO_ONLINE-"--frozen"} \
-    --manifest-path '{}' \;
-
-exit $?
+cd "${abs_top_srcdir}/src/rust" || exit
+exec "${abs_top_builddir}/scripts/cargo_test" --all --all-features
