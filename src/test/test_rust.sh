5f286e4db8ccdb3c35bac959d71ad4ebf5f5ca1e
Isis Lovecruft <isis@torproject.org>
2018-03-23 01:19:39 +0000

    tests: Much quotes. Extremely POSIX. Very find.

--- src/test/test_rust.sh
+++ src/test/test_rust.sh
@@ -3,18 +3,12 @@
 
 set -e
 
-manifests=`find "${abs_top_srcdir:-.}/src/rust/" -mindepth 2 -maxdepth 2 -name 'Cargo.toml'`
+CARGO_TARGET_DIR="${abs_top_builddir:-../../..}/src/rust/target" \
+    CARGO_HOME="${abs_top_builddir:-../../..}/src/rust" \
+    find "${abs_top_srcdir:-../../..}/src/rust" \
+    -mindepth 2 -maxdepth 2 \
+    -type f -name 'Cargo.toml' \
+    -exec "${CARGO:-cargo}" test --all-features ${CARGO_ONLINE-"--frozen"} \
+    --manifest-path '{}' \;
 
-exitcode=0
-
-for manifest in $manifests; do
-    cd "${abs_top_builddir:-../../..}/src/rust"
-    CARGO_TARGET_DIR="${abs_top_builddir:-../../..}/src/rust/target" \
-      CARGO_HOME="${abs_top_builddir:-../../..}/src/rust" \
-      "${CARGO:-cargo}" test --all-features ${CARGO_ONLINE-"--frozen"} \
-      --manifest-path "${manifest}" \
-	|| exitcode=1
-    cd -
-done
-
-exit $exitcode
+exit $?
